c1a175e824daded24fb7d869d450e1333aad31a4
7Ds7 <dinisimoes@gmail.com>
2017-12-02 22:06:47 +0100

    testing rope

---
+++ Assets/Scripts/CraneAnchorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneAnchorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = Input.mousePosition;
+		Debug.Log (pos.y);
+		pos = Camera.main.ScreenToWorldPoint(pos);
+		transform.position = new Vector3 (pos.x, transform.position.y, transform.position.z);
+
+	}
+}
