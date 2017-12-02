50d004c5c47eb0f6b14cb79aea979141d0c85ac5
7Ds7 <dinisimoes@gmail.com>
2017-12-02 22:24:51 +0100

    testing rope

--- Assets/Scripts/CraneAnchorController.cs
+++ Assets/Scripts/CraneAnchorController.cs
@@ -4,17 +4,61 @@ using UnityEngine;
 
 public class CraneAnchorController : MonoBehaviour {
 
+	Tentacle tentacle;
+
 	// Use this for initialization
 	void Start () {
-		
+		tentacle = gameObject.GetComponent<Tentacle>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		var pos = Input.mousePosition;
-		Debug.Log (pos.y);
 		pos = Camera.main.ScreenToWorldPoint(pos);
 		transform.position = new Vector3 (pos.x, transform.position.y, transform.position.z);
+
+		int int_y = (int) pos.y;
+		Debug.Log (int_y);
+		switch (int_y)
+		{
+		case 5:
+			tentacle._setting.length = 0.1f;
+			break;
+		case 4:
+			tentacle._setting.length = 0.2f;
+			break;
+		case 3:
+			tentacle._setting.length = 0.3f;
+			break;
+		case 2:
+			tentacle._setting.length = 0.4f;
+			break;
+		case 1:
+			tentacle._setting.length = 0.5f;
+			break;
+		case 0:
+			tentacle._setting.length = 0.6f;
+			break;
+		case -1:
+			tentacle._setting.length = 0.7f;
+			break;
+		case -2:
+			tentacle._setting.length = 0.8f;
+			break;
+		case -3:
+			tentacle._setting.length = 0.9f;
+			break;
+		case -4:
+			tentacle._setting.length = 1f;
+			break;
+		case -5:
+			tentacle._setting.length = 1.1f;
+			break;
+		default:
+			print ("Incorrect intelligence level.");
+			break;
+		}
+
 
 	}
 }
