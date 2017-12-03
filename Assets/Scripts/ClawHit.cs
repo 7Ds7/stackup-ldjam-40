58e6e6ff094cc4b9c65bf19db09b146ff8bb1ee7
7Ds7 <dinisimoes@gmail.com>
2017-12-03 09:01:42 +0100

    gleft and gright claw close

--- Assets/Scripts/ClawHit.cs
+++ Assets/Scripts/ClawHit.cs
@@ -16,10 +16,22 @@ public class ClawHit : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
-		hit = true;
+		if (coll.gameObject.tag == "Shape") {
+			hit = true;
+			if (gameObject.name == "ClawLeft")
+				coll.gameObject.GetComponent<PieceController> ().gLeft = true;
+			if (gameObject.name == "ClawRight")
+				coll.gameObject.GetComponent<PieceController> ().gRight = true;
+		}
 	}
 
 	void OnCollisionExit2D(Collision2D coll) {
-		hit = false;
+		if (coll.gameObject.tag == "Shape") {
+			hit = false;
+			if (gameObject.name == "ClawLeft")
+				coll.gameObject.GetComponent<PieceController> ().gLeft = false;
+			if (gameObject.name == "ClawRight")
+				coll.gameObject.GetComponent<PieceController> ().gRight= false;
+		}
 	}
 }
--- Assets/Scripts/PieceController.cs
+++ Assets/Scripts/PieceController.cs
@@ -6,14 +6,22 @@ public class PieceController : MonoBehaviour {
 
 	public bool grabbed;
 	// Use this for initialization
+
+	public bool gLeft;
+	public bool gRight;
 	void Start () {
 		grabbed = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (grabbed) {
-			transform.localPosition = GameObject.Find("Claw").transform.localPosition;
+		if (gLeft && gRight) {
+			Debug.Log ("entraaa");
+			transform.SetParent(GameObject.Find("CraneClaw").transform);
 		}
+			
+//		if (grabbed) {
+//			transform.localPosition = GameObject.Find("Claw").transform.localPosition;
+//		}
 	}
 }
