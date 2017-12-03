a280b0a2e6547304b89f1ba66db8338b68cf83a6
7Ds7 <dinisimoes@gmail.com>
2017-12-03 06:21:43 +0100

    cat

---
+++ Assets/Scripts/CatController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour {
+
+	public float speed = 50f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var right = inputState.GetButtonValue (inputButtons [0]);
+		var left = inputState.GetButtonValue (inputButtons [1]);
+		var run = inputState.GetButtonValue (inputButtons [2]);
+
+		if (right || left) {
+			var tmpSpeed = speed;
+			if (run && runMultiplier > 0) {
+				tmpSpeed *= runMultiplier;
+			}
+			var velX = tmpSpeed * (float)inputState.direction;
+			body2d.velocity = new Vector2 (velX, body2d.velocity.y);
+		}
+		
+	}
+}
+
+
+
